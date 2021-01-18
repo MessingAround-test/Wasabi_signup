60a9650cffd7c273e994d6bebc9f1a91355fbb08
lombr <lombryn@gmail.com>
2021-01-18 19:36:42 +1100

    SAVE prior to complete code restructure

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import Button from 'react-bootstrap/Button';
+ 
 
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import QRCodeShower from './Components/QRCodeShower';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 firebase.initializeApp({
   apiKey: "AIzaSyAavR_oNAX7acSEr5-0wV6WBKQpDNaVHrM",
@@ -193,15 +203,19 @@ function SignUpSheet(){
     <Form.Group controlId="BasicInputForm">
       <Form.Label>University Degree</Form.Label>
       <Form.Control type="text" placeholder='Degree Name' />
-      <Form.Text className="text-muted">
+      {/* <Form.Text className="text-muted">
         We'll never share your email with anyone else.
-      </Form.Text>
+      </Form.Text> */}
       <Form.Label>Name</Form.Label>
       <Form.Control type="text" placeholder={auth.currentUser.displayName} onChange={handleChangeName}/> 
-
+    
      
     </Form.Group>
-  
+    <Router>
+    <Route path="/QRCode">
+      <QRCodeShower />
+    </Route>
+    </Router>
     
     <Button variant="primary" type="submit">
       Submit
@@ -212,66 +226,5 @@ function SignUpSheet(){
   
 );
 }
-
-// Under here is the stuff from previously
-function ChatRoom() {
-  const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const [formValue, setFormValue] = useState('');
-
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL
-    })
-
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  return (<>
-    <main>
-
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-      <span ref={dummy}></span>
-
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <Button type="submit" disabled={!formValue}>🕊️</Button>
-
-    </form>
-  </>)
-}
-
-
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
-
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
-    </div>
-  </>)
-}
-
 
 export default App;
